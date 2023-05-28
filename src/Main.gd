38623876d3296da88d33d94271e0f51297774b73
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-05-28 13:59:08 +0900

    draft: call OpenAi API as HTTPRequest

--- src/Main.gd
+++ src/Main.gd
@@ -1,29 +1,20 @@
 extends Node2D
 
-var DIR = OS.get_executable_path().get_base_dir()
-var interpreter_path = DIR.plus_file("python/venv/bin/python3.10")
-var script_path = DIR.plus_file("python/src/talk.py")
-
 var dialogues = []
 var dialogue_index = 0
 
 onready var dialogue_label = $CanvasLayer/Dialogue
 onready var user_input = $CanvasLayer/UserInput
 onready var save_input = $CanvasLayer/SaveInput
+
 onready var save_manager = preload("res://src/SaveManager.gd").new()
+onready var openai_api = preload("res://src/OpenAIApi.gd").new()
 
 func _ready():
-	if !OS.has_feature("standalone"): # if NOT exported version
-		interpreter_path = ProjectSettings.globalize_path("python/venv/bin/python3.10")
-		script_path = ProjectSettings.globalize_path("res://python/src/talk.py")
+	add_child(openai_api)
+	openai_api.connect("response_received", self, "_on_response_received")
 
 	display_dialogue()
-
-func _on_TalkButton_pressed():
-	talk(user_input.text)
-
-func _on_SaveButton_pressed():
-	save_manager.save_api(save_input.text)
 
 func display_dialogue():
 	if dialogue_index < dialogues.size():
@@ -32,15 +23,16 @@ func display_dialogue():
 		print("End of the dialogue")
 
 func talk(user_text: String):
-	print("talk started")
-	var output = []
 	var api_key = save_manager.load_api()
+	openai_api.call_api(api_key, user_text)
 
-	var exit_code = OS.execute(interpreter_path, [script_path, user_text, api_key], true, output)
-	if exit_code == 0:
-		print("Python talk script output: ", output)
-		dialogues = output
-		dialogue_index = 0
-		display_dialogue()
-	else:
-		print("Python talk script error. Exit code: ", exit_code)
+func _on_TalkButton_pressed():
+	talk(user_input.text)
+
+func _on_SaveButton_pressed():
+	save_manager.save_api(save_input.text)
+
+func _on_response_received(response):
+	dialogues = response
+	dialogue_index = 0
+	display_dialogue()
---
+++ src/OpenAIApi.gd
@@ -0,0 +1,29 @@
+extends HTTPRequest
+
+var openai_url = "https://api.openai.com/v1/chat/completions"
+
+func _init():
+	connect("request_completed", self, "_on_request_completed")
+
+func call_api(api_key: String, message: String, max_tokens: int = 100):
+	var headers = [
+		"Authorization: Bearer " + api_key,
+		"Content-Type: application/json",
+	]
+
+	var postData = {
+		"model": "gpt-3.5-turbo",
+		"messages": [{"role": "user", "content": message}],
+		"max_tokens": max_tokens
+	}
+
+	var error = self.request(openai_url, headers, false, HTTPClient.METHOD_POST, JSON.print(postData))
+	if error != OK:
+		print("HTTP request failed with error: ", error)
+
+func _on_request_completed(result, response_code, headers, body):
+	var response = JSON.parse(body.get_string_from_utf8())
+	if "choices" in response.result and response.result["choices"].size() > 0:
+		emit_signal("response_received", response.result.choices[0].message.content.split("\n"))
+	else:
+		emit_signal("response_received", [])
